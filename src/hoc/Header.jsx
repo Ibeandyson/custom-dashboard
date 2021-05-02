469197875426124a771e8c606756bd6ceb9965da
Ibeandyson <ibeandyson123@gmail.com>
2021-05-02 21:49:42 +0100

    done sekeleton of dashboard

--- src/hoc/Header.jsx
+++ src/hoc/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
                 {/* <a className="navbar-brand" style={{ backgroundColor: "#ffffff", height: '60px', width: "60px", borderRadius: "50px"}}>
                     <img src={Logo} style={{height: '50px'}} alt="log" />
                 </a> */}
-                <a className="navbar-brand" />
+                <a className="navbar-brand">
+                    <h5 style={{fontSize: '0.9em', fontWeight: 'bolder', paddingLeft: "40px"}}>Admin</h5>
+                </a>
 
                 <button className="btn btn-sm btn-danger my-2 my-sm-0" type="submit">
                     Logout
--- src/hoc/SideBar.jsx
+++ src/hoc/SideBar.jsx
@@ -1,5 +1,6 @@
 import './Styles/SideBar.css';
-import {FaTachometerAlt} from 'react-icons/fa';
+import {FaTachometerAlt, FaUserLock, FaUserTie,FaCog} from 'react-icons/fa';
+import {ImUsers, ImUserPlus} from 'react-icons/im';
 
 const SideBar = () => {
     return (
@@ -7,8 +8,28 @@ const SideBar = () => {
             <div className="card side-bar-card shadow border-0">
                 <div className="side-card-body">
                     <div className="side-link-container">
-                        <FaTachometerAlt style={{fontSize: '1em', marginRight: '10px'}} />
+                        <FaTachometerAlt style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
                         <span className="side-link-text">DashBoard</span>
+                    </div>
+                    <div className="side-link-container">
+                        <ImUsers style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
+                        <span className="side-link-text">Active Users</span>
+                    </div>
+                    <div className="side-link-container">
+                        <FaUserLock style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
+                        <span className="side-link-text">Blocked Users</span>
+                    </div>
+                    <div className="side-link-container">
+                        <ImUserPlus style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
+                        <span className="side-link-text">Add Use</span>
+                    </div>
+                    <div className="side-link-container">
+                        <FaUserTie style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
+                        <span className="side-link-text">Profile</span>
+                    </div>
+                    <div className="side-link-container">
+                        <FaCog style={{fontSize: '1em', marginRight: '10px', color: "#ffffff"}} />
+                        <span className="side-link-text">Settings</span>
                     </div>
                 </div>
             </div>
