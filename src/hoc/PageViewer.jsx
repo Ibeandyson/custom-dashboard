f5b23868f474dd975ebd46f5ab118921724da943
Ibeandyson <ibeandyson123@gmail.com>
2021-05-03 07:28:31 +0100

    added  hover on sideabar

--- src/hoc/PageViewer.jsx
+++ src/hoc/PageViewer.jsx
@@ -1,7 +1,6 @@
 import {useState, useEffect} from 'react';
 
 const PageViewer = () => {
-  const { height} = useWindowDimensions();
     return (
         <div>
             <p>This is page viewer</p>
@@ -11,26 +10,3 @@ const PageViewer = () => {
 export default PageViewer;
 
 
-
-function getWindowDimensions() {
-    const {innerWidth: width, innerHeight: height} = window;
-    return {
-        width,
-        height
-    };
-}
-
-const useWindowDimensions = () => {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-};
